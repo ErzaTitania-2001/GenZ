6a730dbb3ab706bf505c95e901da04d54447a16e
Ahana Pal <59911272+ErzaTitania-2001@users.noreply.github.com>
2021-10-21 12:04:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import dotenv
+from dotenv import load_dotenv
+import os
+import os
+
+#import all of the cogs
+from main_cog import main_cog
+#from image_cog import image_cog
+from music_cog import music_cog
+TOKEN="OTAwMzM4MjU1MjQ4MTIxODg2.YW_3Vw.2MqKcMHMTLKK6nj7XwmUJnkgKf0"
+bot = commands.Bot(command_prefix='Z')
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+bot.add_cog(main_cog(bot))
+#bot.add_cog(image_cog(bot))
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+bot.run(TOKEN)
+#bot.run(os.getenv("TOKEN"))
