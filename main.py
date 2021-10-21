5f7f016a064868e71ad335e285c801685d783c57
Ahana Pal <59911272+ErzaTitania-2001@users.noreply.github.com>
2021-10-22 00:20:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import os
 
 #import all of the cogs
 from main_cog import main_cog
-#from image_cog import image_cog
 from music_cog import music_cog
 
 bot = commands.Bot(command_prefix='Z')
@@ -17,7 +16,6 @@ bot.remove_command('help')
 
 #register the class with the bot
 bot.add_cog(main_cog(bot))
-#bot.add_cog(image_cog(bot))
 bot.add_cog(music_cog(bot))
 
 #start the bot with our token
