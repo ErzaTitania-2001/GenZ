ab7071551ad00445c83a0f2a2a69229c60e65dc0
Ahana Pal <59911272+ErzaTitania-2001@users.noreply.github.com>
2021-10-22 17:12:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 #import all of the cogs
 from main_cog import main_cog
 from music_cog import music_cog
-
+TOKEN="OTAwMzM4MjU1MjQ4MTIxODg2.YW_3Vw.bW44n6RcbSokdO58XxaUAUOHgXk"
 bot = commands.Bot(command_prefix='Z')
 
 #remove the default help command so that we can write out own
@@ -19,5 +19,23 @@ bot.add_cog(main_cog(bot))
 bot.add_cog(music_cog(bot))
 
 #start the bot with our token
+
+@bot.event
+async def on_ready():
+    print('Hello, I am your music bot {0.user}'
+    .format(bot))
+    generel_channel= bot.get_channel(882939306870714408) #897442493338124328
+
+    await generel_channel.send('Yo! Wot bussin? GenZ here.Type Zhelp for further info.'
+    .format(bot))
+@bot.event
+async def on_message(message):
+    if message.content.startswith('Zgo'): # bot leave channel
+        if (message.guild.voice_client): # If the bot is in a voice channel 
+            await message.guild.voice_client.disconnect() # Leave the channel
+            await message.channel.send('Peace out, brother.')
+        else: # But if it isn't
+            await message.channel.send("I'm not in a voice channel, shawty. Play something. Use Zhelp. It helps.")
+    await bot.process_commands(message) # commands work properly
 bot.run(TOKEN)
 #bot.run(os.getenv("TOKEN"))
